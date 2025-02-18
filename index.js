5dfa93e89913009a0d64c82bf203da3d1fe08bcf
조민주 <pida6716@gmail.com>
2025-02-18 18:14:02 +0900

    Update readme index.js

--- index.js
+++ index.js
@@ -7,36 +7,45 @@ import Parser from 'rss-parser';
  */
 let text = `
 
-[![Typing SVG](https://readme-typing-svg.demolab.com?font=Sriracha&weight=400&size=60&duration=5001&pause=1000&color=6A5ACD&center=true&vCenter=true&width=900&height=300&lines=Hello+World!+I'm+Minz+👀;)](https://git.io/typing-svg)
+
+
+[![Typing SVG](https://readme-typing-svg.demolab.com?font=Sriracha&weight=400&size=60&duration=5001&pause=1000&color=6A5ACD&center=true&vCenter=true&width=900&height=300&lines=Hello+World!+I'm+Minz+👀;Striving+for+learnable+code+🧐;)](https://git.io/typing-svg)
 
 
 
-<summary>😎 <b>developers/Mimiminz</b></summary>
+<div align="center">
+  <h2>📚TECH STACK📚</h2>
+  <h4>My Primary Stack</h4>
+  <img src="https://img.shields.io/badge/React-61DAFB?style=flat&logo=React&logoColor=white"> <img src="https://img.shields.io/badge/Tanstack query-FF4154?style=flat&logo=reactquery&logoColor=white"> <img src="https://img.shields.io/badge/Zustand-443e38?style=flat">
+  
+  <img src="https://img.shields.io/badge/TypeScript-3178C6?style=flat&logo=typescript&logoColor=white"> <img src="https://img.shields.io/badge/Javascript-F7DF1E?style=flat&logo=javascript&logoColor=white"> <img src="https://img.shields.io/badge/Styled components-DB7093?style=flat&logo=styledcomponents&logoColor=white">
 
+  <h4>Experience</h4>
+  <img src="https://img.shields.io/badge/Next.js-000000?style=flat&logo=nextdotjs&logoColor=white"> <img src="https://img.shields.io/badge/Vue.js-4FC08D?style=flat&logo=vuedotjs&logoColor=white">
+  
+  <img src="https://img.shields.io/badge/Vite-646CFF?style=flat&logo=vite&logoColor=white"> <img src="https://img.shields.io/badge/Tailwind-06B6D4?style=flat&logo=tailwindcss&logoColor=white"> <img src="https://img.shields.io/badge/Emotion-d36ac2?style=flat">
+</div> 
 
+<br/>
 
-#### 📊 Recent My Activity..
-<hr/>
+<div align="center">
+  <h2>GITHUB STATE</h2>
 
 [![Mimiminz's github activity graph](https://github-readme-activity-graph.vercel.app/graph?username=Mimiminz&bg_color=020715&color=8A2BE2&line=8A2BE2&point=8977ad&area=true&hide_border=true)](https://github.com/ashutosh00710/github-readme-activity-graph)
 
-<div align="center">
 
 [![Mimiminz's GitHub stats](https://github-readme-stats.vercel.app/api?username=Mimiminz&show_icons=true&bg_color=020715&title_color=8A2BE2&icon_color=928AFA&text_color=fdfdfd&rank_icon=github&hide_border=true&text_bold=false)](https://github.com/Mimiminz/github-readme-stats)
 ![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=Mimiminz&layout=compact&hide_border=true&bg_color=020715&card_width=350&text_color=E6E5FA&title_color=6A5ACD&langs_count=8)
 
-</div>
-
-<hr/>
-<br/>  
-<div align="center">
 <img src="https://komarev.com/ghpvc/?username=Mimiminz&&style=flat-square&color=8A2BE2"/>
 </div>  
+
+<br/>
   
-<br/>  
+<div align="center">
+  <h2>MEMORIES</h2>
 
-## 📕 Latest Blog Posts
-
+</div>
 `;
 
 // rss-parser 생성
@@ -50,7 +59,7 @@ const parser = new Parser({
   // 피드 목록
   const feed = await parser.parseURL('https://ciaom.tistory.com/rss'); // 본인의 블로그 주소
 
-  text += `<ul>`;
+  text += `<div>`;
 
   // 최신 5개의 글의 제목과 링크를 가져온 후 text에 추가
   const postCount = Math.min(feed.items.length, 5);
@@ -59,10 +68,10 @@ const parser = new Parser({
     console.log(`${i + 1}번째 게시물`);
     console.log(`추가될 제목: ${title}`);
     console.log(`추가될 링크: ${link}`);
-    text += `<li><a href='${link}' target='_blank'>${title}</a></li>`;
+    text += `<p><a href='${link}' target='_blank'>${title}</a></p>`;
   }
 
-  text += `</ul>`;
+  text += `</div></div>`;
 
   // README.md 파일 생성
   writeFileSync('README.md', text, 'utf8', (e) => {
